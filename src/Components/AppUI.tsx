eaf1858e4ffe8269f03b9e7cf60f4fc89adaf4d8
ricardocollado <ricardocolladorothschild@gmail.com>
2024-03-28 19:29:53 -0400

    Remove old useEffect that was helping for an intitial state on the list, for styling porpuse

--- src/Components/AppUI.tsx
+++ src/Components/AppUI.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import BalanceComponent from '../Components/BalanceComponent';
 import { AddNewComponent } from './AddNewComponent';
 import { ExpenseType } from './ExpenseType';
@@ -10,21 +10,7 @@ export default function AppUI(){
 
     const [expenseList, setExpenseList] = useState<Array<ExpenseType>>([]);
 
-    
 
-    useEffect(()=>{
-        setExpenseList([
-            {
-                expense:"comer",
-                amount:300
-            },
-            {
-                expense:"jugar",
-                amount:100
-            
-            }
-        ]);
-    },[]);
     
     return(
         <main>
